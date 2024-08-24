76a2dccaf8a41223bd9346f434800d4380fd3cc7
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-08-24 19:12:24 +0200

    Fix double rating issue

--- src/components/starRating.jsx
+++ src/components/starRating.jsx
@@ -4,6 +4,7 @@ import Rating from "react-rating";
 const StarRating = ({ currentRating, quantity }) => {
   const [rating, setRating] = useState(currentRating);
   const [yourRating, setYourRating] = useState(0);
+  const [prevRating, setPrevRating] = useState(0);
   const [reviewNumber, setReviewNumber] = useState(quantity);
   const [rated, setRated] = useState(false);
   console.log(reviewNumber);
@@ -19,10 +20,40 @@ const StarRating = ({ currentRating, quantity }) => {
         fullSymbol="fa fa-star fa-2x"
         fractions={2}
         initialRating={currentRating}
+        // {rated === false ? 0 : 5}
         onChange={(rate) => {
-          setRating((rating * reviewNumber + rate) / (reviewNumber + 1));
-          setReviewNumber(reviewNumber + 1);
+          if (rated === false) {
+            // Handle the case where rated is false
+            setRating((rating * reviewNumber + rate) / (reviewNumber + 1));
+            setReviewNumber(reviewNumber + 1); // Assuming this is the first rating
+            setRated(true);
+            setYourRating(rate);
+          } else {
+            setPrevRating(yourRating);
+            setYourRating(rate);
+            setRating(
+              (rating * reviewNumber - prevRating + rate) / reviewNumber
+            );
+            // Handle the case where rated is true
+            // setRating((rating * reviewNumber + rate) / (reviewNumber + 1));
+            // setReviewNumber(reviewNumber + 1);
+          }
+
+          // setRating(
+          //   rated === false
+          //     ? rate
+          //     : (rating * reviewNumber + rate) / (reviewNumber + 1)
+          // );
+          // setReviewNumber(rated === false ? reviewNumber : reviewNumber + 1);
+
+          // setRated(rated ? true : false)
         }}
+      />
+      <Rating
+        emptySymbol="fa fa-star-o fa-2x"
+        fullSymbol="fa fa-star fa-2x"
+        fractions={2}
+        initialRating={yourRating}
       />
       <p>Your rating: {yourRating}</p>
       <p>Total rating: {rating}</p>
@@ -32,3 +63,8 @@ const StarRating = ({ currentRating, quantity }) => {
 };
 
 export default StarRating;
+
+// onChange={(rate) => {
+//   setRating(rated === false ? rate : (rating * reviewNumber + rate) / (reviewNumber + 1));
+//   setReviewNumber(rated === false ? 1 : reviewNumber + 1);
+// }}
